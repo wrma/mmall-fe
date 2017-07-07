f56b22a221f9d980c4faa893488cc1ff7216e27b
wrma <632498767@qq.com>
2017-07-07 16:19:39 +0800

    添加font-awesome

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,9 +2,10 @@
 * @Author: ThinkPad
 * @Date:   2017-06-30 21:04:25
 * @Last Modified by:   ThinkPad
-* @Last Modified time: 2017-07-07 10:57:17
+* @Last Modified time: 2017-07-07 16:17:50
 */
 
 'use strict';
 
 require('./layout.css');
+require('node_modules/font-awesome/css/font-awesome.min.css');
