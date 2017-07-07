60a27a1e63b40bb0d137c67a0822fd04827c809f
wrma <632498767@qq.com>
2017-07-07 22:34:07 +0800

    搜索框样式添加

---
+++ src/page/common/header/index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: ThinkPad
+* @Date:   2017-07-07 21:18:23
+* @Last Modified by:   ThinkPad
+* @Last Modified time: 2017-07-07 22:32:27
+*/
+
+'use strict';
+
+require('./index.css');
+
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,10 +2,10 @@
 * @Author: wrma
 * @Date:   2017-06-30 18:58:21
 * @Last Modified by:   ThinkPad
-* @Last Modified time: 2017-07-07 16:39:52
+* @Last Modified time: 2017-07-07 21:25:52
 */
 'use strict';
-require('page/common/nav/index.js');
+require('page/common/header/index.js');
 
 var _mm = require('util/mm.js');
 
