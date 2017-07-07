1ff15e66db9d1cb53526b70c2693e257852a1311
wrma <632498767@qq.com>
2017-07-07 17:19:56 +0800

    完善footer

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,10 +2,11 @@
 * @Author: ThinkPad
 * @Date:   2017-06-30 21:04:25
 * @Last Modified by:   ThinkPad
-* @Last Modified time: 2017-07-07 16:17:50
+* @Last Modified time: 2017-07-07 17:08:07
 */
 
 'use strict';
 
 require('./layout.css');
 require('node_modules/font-awesome/css/font-awesome.min.css');
+require('./footer/index.css');
