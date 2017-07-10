345f96fe042a7d78eb2f57d31607d2a33352a632
wrma <632498767@qq.com>
2017-07-10 19:27:34 +0800

    简易结果页的完成

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -2,7 +2,18 @@
 * @Author: ThinkPad
 * @Date:   2017-07-10 18:25:51
 * @Last Modified by:   ThinkPad
-* @Last Modified time: 2017-07-10 18:33:55
+* @Last Modified time: 2017-07-10 19:26:15
 */
 
 'use strict';
+
+require('./index.css');
+require('page/common/nav-simple/index.js');
+var _mm = require('util/mm.js');
+
+$(function(){
+    var type = _mm.getUrlParam('type') || 'default',
+    element = $('.'+ type + '-success');
+    //显示提示元素
+    element.show();
+})
